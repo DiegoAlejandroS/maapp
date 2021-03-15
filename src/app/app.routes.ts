563e4253007e802d5d3fea99f79c06829735566b
Supralejandro <diego.segura.9708@gmail.com>
2021-03-15 14:32:39 -0500

    Se agrega ruta de pagina tareas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/pages/home/home.component';
+import {TabletasksComponent} from './components/shared/tabletasks/tabletasks.component';
 
 
 
 const APP_ROUTES: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: '**', pathMatch: 'full', redirectTo: ''}
+    {path: 'tabletasks', component: TabletasksComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
