12ef1e60ac6f32183c8a1e784e80eb74243c7804
Supralejandro <diego.segura.9708@gmail.com>
2021-03-15 14:35:15 -0500

    Se importan los componentes y el servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
-import {APP_ROUTING} from './app.routes';
+import { APP_ROUTING } from './app.routes';
+
+import { DataService } from './services/data.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { TabletasksComponent } from './components/shared/tabletasks/tabletasks.component';
+import { TaskComponent } from './components/shared/task/task.component';
+import { AddtaskComponent } from './components/shared/addtask/addtask.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    SidebarComponent
+    SidebarComponent,
+    TabletasksComponent,
+    TaskComponent,
+    AddtaskComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
